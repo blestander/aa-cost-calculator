fc77c7ec5d1400b4e4d55be59e1da0fe2e7314b6
Brendan Lestander <blestander@pitt.edu>
2020-04-25 11:34:15 -0400

    Populated ItemCountComponent.

--- src/app/item-count/item-count.component.ts
+++ src/app/item-count/item-count.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Change } from '../change';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-item-count',
@@ -6,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./item-count.component.css']
 })
 export class ItemCountComponent implements OnInit {
+
+    @Output() change = new EventEmitter<Change>();
+
+    countControl = new FormControl('');
 
     constructor() { }
 
