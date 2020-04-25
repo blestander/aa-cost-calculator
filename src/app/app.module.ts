6d87da0074ea558672fd46e5d4aa548882b7939c
Brendan Lestander <blestander@pitt.edu>
2020-04-25 10:37:41 -0400

    Created skeleton item component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { VersionSelectorComponent } from './version-selector/version-selector.component';
 import { CostCalculatorComponent } from './cost-calculator/cost-calculator.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         VersionSelectorComponent,
         CostCalculatorComponent,
+        ItemComponent,
     ],
     imports: [
         BrowserModule,
