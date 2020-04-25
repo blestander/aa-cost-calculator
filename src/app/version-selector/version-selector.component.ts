f3dfe00a619cc8188bc475c9c3d75243470a0516
Brendan Lestander <blestander@pitt.edu>
2020-04-25 10:07:31 -0400

    Corrected spacing

--- src/app/version-selector/version-selector.component.ts
+++ src/app/version-selector/version-selector.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-version-selector',
-  templateUrl: './version-selector.component.html',
-  styleUrls: ['./version-selector.component.css']
+    selector: 'app-version-selector',
+    templateUrl: './version-selector.component.html',
+    styleUrls: ['./version-selector.component.css']
 })
 export class VersionSelectorComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
