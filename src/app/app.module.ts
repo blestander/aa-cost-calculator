a7ba4771966ab1565c5a7b30aa1d42a24e0018d8
Brendan Lestander <blestander@pitt.edu>
2020-04-25 10:30:58 -0400

    Created skeleton CostCalculatorComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { VersionSelectorComponent } from './version-selector/version-selector.component';
+import { CostCalculatorComponent } from './cost-calculator/cost-calculator.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         VersionSelectorComponent,
+        CostCalculatorComponent,
     ],
     imports: [
         BrowserModule,
