0d885a4b6e6c30c4375e1d8b2549999ef36331a4
Brendan Lestander <blestander@pitt.edu>
2020-05-19 14:59:47 -0400

    Built RepairsComponent and styled it for desktop.

--- src/app/repairs/repairs.component.ts
+++ src/app/repairs/repairs.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Change } from '../change';
 
 @Component({
     selector: 'app-repairs',
     templateUrl: './repairs.component.html',
     styleUrls: ['./repairs.component.css']
 })
-export class RepairsComponent implements OnInit {
+export class RepairsComponent implements OnInit, OnChanges {
+
+    @Input() resetCount: number;
+    @Output() changedSubtotal = new EventEmitter<Change>();
+
+    subtotal: number = 0;
+    internalResetCount: number;
 
     constructor() { }
 
     ngOnInit(): void {
+        this.internalResetCount = this.resetCount;
     }
 
+    ngOnChanges(): void {
+        if (this.resetCount != this.internalResetCount) {
+            this.internalResetCount = this.resetCount;
+            this.subtotal = 0;
+        }
+    }
+
+    onCountChange(newCount: number) {
+        let old = this.subtotal
+        this.subtotal = newCount;
+        this.changedSubtotal.emit({
+            old: old,
+            current: this.subtotal
+        })
+    }
 }
